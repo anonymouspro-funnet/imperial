5d99584b11a99e2f7c74420d154f0cfad7d59a18
anonymouspro-funnet <samuelnjengakamau@gmail.com>
2022-08-06 00:45:55 +0100

    Initial Commit

---
+++ resources/views/starships.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Venus Template</title>
+    <link href="https://fonts.googleapis.com/css?family=Heebo:400,700|Oxygen:700" rel="stylesheet">
+    <link rel="stylesheet" href="/css/style.css">
+    <script src="https://unpkg.com/scrollreveal@4.0.5/dist/scrollreveal.min.js"></script>
+</head>
+<body class="is-boxed has-animations hero-particles-container">
+<div class="body-wrap boxed-container">
+    <div class="container-sm">
+        <div class="hero-inner">
+            <div class="hero-copy">
+                <h1 class="hero-title mt-0">Imperial Galactic Fleet</h1>
+            </div>
+        </div>
+    </div>
+    <canvas id="hero-particles"></canvas>
+    <div class="footer-particles-container">
+            <canvas id="footer-particles"></canvas>
+    </div>
+</div>
+<script src="/js/main.min.js"></script>
+</body>
+</html>
